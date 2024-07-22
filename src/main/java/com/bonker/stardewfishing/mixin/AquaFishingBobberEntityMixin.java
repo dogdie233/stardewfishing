6ab1d13aa191c66dee6db4876cb52f8a2d228efe
BrownBear85 <91921122+BrownBear85@users.noreply.github.com>
2024-07-22 10:27:30 -0700

    removed unnecessary code

--- src/main/java/com/bonker/stardewfishing/mixin/AquaFishingBobberEntityMixin.java
+++ src/main/java/com/bonker/stardewfishing/mixin/AquaFishingBobberEntityMixin.java
@@ -3,7 +3,6 @@ package com.bonker.stardewfishing.mixin;
 import com.bonker.stardewfishing.StardewFishing;
 import com.bonker.stardewfishing.common.FishingHookLogic;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundSource;
